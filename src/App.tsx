78cf38ff8a7467eec0e22fee95780e3442128524
MARCOS VINICIUS DO NASCIMENTO SANTOS <marcos.santos@armazempb.com.br>
2024-07-09 13:35:39 -0300

    feat: create initial structure of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
 export function App() {
-  return <div>App</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-full max-w-3xl text-center">
+        <p className="text-zinc-300 text-lg">
+          Convide seus amigos e planeje sua próxima viagem!
+        </p>
+
+        <div>
+          <input type="text" placeholder="Para onde você vai?" />
+          <input type="text" placeholder="Quando?" />
+          <button> Continuar </button>
+        </div>
+
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda{" "}
+          <br /> com nossos{" "}
+          <a href="#" className="text-zinc-300 underline">
+            {" "}
+            termos de uso{" "}
+          </a>{" "}
+          e{" "}
+          <a href="#" className="text-zinc-300 underline">
+            {" "}
+            políticas de privacidade{" "}
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
